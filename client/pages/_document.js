e594c050c9b5e4fbd5331bb0fc7250f630bfbc36
JoelSaleem <joeljsaleem@gmail.com>
2020-06-14 14:48:48 +0100

    :sparkles: Tachyons setup

---
+++ client/pages/_document.js
@@ -0,0 +1,27 @@
+import Document, { Head, Main, NextScript } from 'next/document'
+import React from 'react'
+
+export default class MyDocument extends Document {
+  static getInitialProps ({ renderPage }) {
+    const { html, head, errorHtml, chunks } = renderPage()
+    return { html, head, errorHtml, chunks }
+  }
+
+  render () {
+    return (
+      <html>
+        <Head>
+          <meta name="viewport" content="width=device-width, initial-scale=1" />
+          <link
+            rel="stylesheet"
+            href="https://unpkg.com/tachyons@4.8.0/css/tachyons.min.css"
+          />
+        </Head>
+        <body>
+          <Main />
+          <NextScript />
+        </body>
+      </html>
+    )
+  }
+}
