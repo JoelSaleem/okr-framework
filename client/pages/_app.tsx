233a9c6cdbff4f9b69eef09cae3894e93da33ce7
JoelSaleem <joeljsaleem@gmail.com>
2020-07-05 11:10:53 +0100

    🚀

--- client/pages/_app.tsx
+++ client/pages/_app.tsx
@@ -6,7 +6,7 @@ import { ApolloProvider } from "@apollo/react-hooks";
 export const LOCAL_STORAGE_TOKEN_KEY = "OKR_LS_TOKEN";
 
 const client = new ApolloClient({
-  uri: "http://localhost:3000/api/graphql",
+  uri: "https://okr-framework.vercel.app/api/graphql",
   request: (operation: Operation) => {
     if (typeof window === "undefined") {
       return;
